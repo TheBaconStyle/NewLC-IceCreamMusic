47ab16d3c4b5e97fca205169d76ba30db8dc07a6
TheBaconStyle <maks.christmas2001@gmail.com>
2024-12-26 13:49:06 +0200

    visual changes

--- fonts/index.ts
+++ fonts/index.ts
@@ -1,8 +1,10 @@
-import { Open_Sans, Oswald, Roboto } from "next/font/google";
+import { Open_Sans, Oswald, Roboto, Rubik } from "next/font/google";
 
 export const oswald = Oswald({ weight: ["400", "700"], subsets: ["latin"] });
 
 export const roboto = Roboto({ weight: ["400", "700"], subsets: ["latin"] });
+
+export const rubik = Rubik({ subsets: ["latin"] });
 
 export const open = Open_Sans({
   weight: ["400", "500", "600", "700"],
